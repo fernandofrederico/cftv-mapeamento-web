eefe18453d195f7372a3c34d4ba781bf22fe0b3c
Fernando Frederico <fernandofrederico@users.noreply.github.com>
2026-08-02 23:53:26 +0000

    Adiciona fotos reais das caixas (painel e switch) migradas do sistema antigo

---
+++ sql/003_fotos_caixas.sql
@@ -0,0 +1,17 @@
+SET NAMES utf8mb4;
+
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-5-painel.jpg', foto_switch = '/fotos-caixas/caixa-5-switch.jpg' WHERE id = 5;
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-6-painel.jpg', foto_switch = '/fotos-caixas/caixa-6-switch.jpg' WHERE id = 6;
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-7-painel.jpg', foto_switch = '/fotos-caixas/caixa-7-switch.jpg' WHERE id = 7;
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-8-painel.jpg', foto_switch = '/fotos-caixas/caixa-8-switch.jpg' WHERE id = 8;
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-9-painel.jpg', foto_switch = '/fotos-caixas/caixa-9-switch.jpg' WHERE id = 9;
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-10-painel.jpg', foto_switch = '/fotos-caixas/caixa-10-switch.jpg' WHERE id = 10;
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-11-painel.jpg', foto_switch = '/fotos-caixas/caixa-11-switch.jpg' WHERE id = 11;
+UPDATE caixas SET foto_painel = '', foto_switch = '/fotos-caixas/caixa-12-switch.jpg' WHERE id = 12;
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-13-painel.jpg', foto_switch = '/fotos-caixas/caixa-13-switch.jpg' WHERE id = 13;
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-14-painel.jpg', foto_switch = '/fotos-caixas/caixa-14-switch.jpg' WHERE id = 14;
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-15-painel.jpg', foto_switch = '/fotos-caixas/caixa-15-switch.jpg' WHERE id = 15;
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-16-painel.jpg', foto_switch = '/fotos-caixas/caixa-16-switch.jpg' WHERE id = 16;
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-17-painel.jpg', foto_switch = '/fotos-caixas/caixa-17-switch.jpg' WHERE id = 17;
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-18-painel.jpg', foto_switch = '/fotos-caixas/caixa-18-switch.jpg' WHERE id = 18;
+UPDATE caixas SET foto_painel = '/fotos-caixas/caixa-19-painel.jpg', foto_switch = '/fotos-caixas/caixa-19-switch.jpg' WHERE id = 19;
